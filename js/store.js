062a0e45a1b68a255beb28fc5b5690f595c953b7
Tito Valiente <titolvaliente@gmail.com>
2018-02-21 21:48:44 -0600

    pushing project up

---
+++ js/store.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+  //Javascript Parser
+  function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  // Certain minor words should be left lowercase unless 
+  // they are the first or last words in the string
+  lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+  'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+  for (i = 0, j = lowers.length; i < j; i++)
+    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(txt) {
+        return txt.toLowerCase();
+      });
+  }
+  titleCase();
+ 
+})();
